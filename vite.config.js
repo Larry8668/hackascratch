ab46bbb84dfc37258e590b7438bfd374c6e0662a
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2024-08-29 05:45:04 +0530

    modified env

--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,23 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env.VITE_VERCELFIREBASE_API_KEY": JSON.stringify(
-      process.env.VITE_VERCELFIREBASE_API_KEY
+    "process.env.VITE_VERCEL_FIREBASE_API_KEY": JSON.stringify(
+      process.env.VITE_VERCEL_FIREBASE_API_KEY
     ),
-    "process.env.VITE_VERCELFIREBASE_AUTH_DOMAIN": JSON.stringify(
-      process.env.VITE_VERCELFIREBASE_AUTH_DOMAIN
+    "process.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN": JSON.stringify(
+      process.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN
     ),
-    "process.env.VITE_VERCELFIREBASE_PROJECT_ID": JSON.stringify(
-      process.env.VITE_VERCELFIREBASE_PROJECT_ID
+    "process.env.VITE_VERCEL_FIREBASE_PROJECT_ID": JSON.stringify(
+      process.env.VITE_VERCEL_FIREBASE_PROJECT_ID
     ),
-    "process.env.VITE_VERCELFIREBASE_STORAGE_BUCKET": JSON.stringify(
-      process.env.VITE_VERCELFIREBASE_STORAGE_BUCKET
+    "process.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET": JSON.stringify(
+      process.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET
     ),
-    "process.env.VITE_VERCELFIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
-      process.env.VITE_VERCELFIREBASE_MESSAGING_SENDER_ID
+    "process.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+      process.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID
     ),
-    "process.env.VITE_VERCELFIREBASE_APP_ID": JSON.stringify(
-      process.env.VITE_VERCELFIREBASE_APP_ID
+    "process.env.VITE_VERCEL_FIREBASE_APP_ID": JSON.stringify(
+      process.env.VITE_VERCEL_FIREBASE_APP_ID
     ),
   },
 });
